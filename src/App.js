49b604c25002c2831261154bc318dff8b8f99a30
Christopher <mail@christophermaier.com.au>
2017-11-27 14:38:55 +1000

    Added H1 & H2 to function called Heading. Turned todo into array of hashes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import './App.css';
 import toDos from './data';
 import { Container, Row, Col, ListGroup, ListGroupItem, Badge } from 'reactstrap';
 
+function Heading() {
+  return
+    <div class="heading">;
+      <h1>My Todo list</h1>;
+      <h4>Things I need to get done!</h4>;
+    </div>;
+};
+
 class App extends Component {
   render() {
     return (
-      <ul>
-        {toDos.map(item => (
-          <li>{ item }</li>
-        ))}
-      </ul>
-    );
+      <div className="App">
+        <ul>
+          {toDos.map(item => (
+            <li>{ item }</li>
+          ))}
+        </ul>
+      </div>)
   }
 }
 
